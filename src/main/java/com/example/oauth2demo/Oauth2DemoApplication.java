5c322bcfc2e6ca5496daff8f5574728e61b1236d
Felipe Carrillo <felipe.carrillo1@hotmail.com>
2024-04-03 01:27:28 -0600

    Add security filter chain configuration to avoid defaults. Modify home page to provide an explicit link to log in with GitHub instead of being redirected automatically.

--- src/main/java/com/example/oauth2demo/Oauth2DemoApplication.java
+++ src/main/java/com/example/oauth2demo/Oauth2DemoApplication.java
@@ -2,9 +2,42 @@ package com.example.oauth2demo;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.context.annotation.Bean;
+import org.springframework.http.HttpStatus;
+import org.springframework.security.config.annotation.web.builders.HttpSecurity;
+import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
+import org.springframework.security.core.annotation.AuthenticationPrincipal;
+import org.springframework.security.oauth2.core.user.OAuth2User;
+import org.springframework.security.web.SecurityFilterChain;
+import org.springframework.security.web.authentication.HttpStatusEntryPoint;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+import java.util.Collections;
+import java.util.Map;
+
+import static org.springframework.security.config.Customizer.withDefaults;
 
 @SpringBootApplication
+@RestController
+@EnableWebSecurity
 public class Oauth2DemoApplication {
+	@GetMapping("/user")
+	public Map<String, Object> user(@AuthenticationPrincipal OAuth2User principal) {
+		return Collections.singletonMap("name", principal.getAttribute("name"));
+	}
+
+	@Bean
+	public SecurityFilterChain filterChain(HttpSecurity http) throws Exception {
+		http
+				.authorizeHttpRequests(authz -> authz
+						.requestMatchers("/", "/index.html", "/login", "/error", "/webjars/**").permitAll()
+						.anyRequest().authenticated()
+				)
+				.exceptionHandling(e -> e.authenticationEntryPoint(new HttpStatusEntryPoint(HttpStatus.UNAUTHORIZED)))
+				.oauth2Login(withDefaults());
+		return http.build();
+	}
 
 	public static void main(String[] args) {
 		SpringApplication.run(Oauth2DemoApplication.class, args);
